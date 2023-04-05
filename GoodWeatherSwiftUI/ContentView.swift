47cc65a3365ac0822c508be54cb1d53f5873e81a
Òscar Muntal <oscar.muntal@gmail.com>
2023-04-05 22:46:15 +0200

    Initial Commit

---
+++ GoodWeatherSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GoodWeatherSwiftUI
+//
+//  Created by Òscar Muntal on 5/4/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GoodWeatherSwiftUI/GoodWeatherSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoodWeatherSwiftUIApp.swift
+//  GoodWeatherSwiftUI
+//
+//  Created by Òscar Muntal on 5/4/23.
+//
+
+import SwiftUI
+
+@main
+struct GoodWeatherSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
